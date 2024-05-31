12b5fc435712d849b9046d3023882ec22edbf943
Levanijafaridzee <levanijafaridzejafaridze@gmail.com>
2024-05-31 21:09:54 +0400

    commit

---
+++ Level 6/homework/homework0.py
@@ -0,0 +1,36 @@
+name=input("plss enter your name")
+print("heepy birth dey "+name)
+
+num1=int(input("enter num1:"))
+num2=int(input("enter num2:"))
+
+print(num1+num2)
+
+print(num1-num2)
+
+print(num1*num2)
+
+print(num1/num2)
+
+name=input("pls enter your name:")
+
+age=input("pls enter your age:")
+
+lastname=input("pls enter your lastname:")
+
+print(name)
+
+print(age)
+
+print(lastname)
+
+
+age=int(input("plss enter your age"))
+
+print(age+25)
+
+
+
+
+
+
