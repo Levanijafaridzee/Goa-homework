7475673974edfc38c1bd2606c67ca27d21c0a3fd
Levanijafaridzee <levanijafaridzejafaridze@gmail.com>
2024-06-13 21:11:49 +0400

    commit

---
+++ level 10/homework0.py
@@ -0,0 +1,34 @@
+for i in range (13):
+    print(i)
+
+
+for i in range (14):
+    print(i)
+
+for i in range (20):
+    print(i)
+
+
+
+for i in range (11):
+    print(i)
+
+
+
+
+
+for i in range (18):
+    print(i)
+
+
+
+for i in range (17):
+    print(i)
+
+
+
+
+
+for i in range (19):
+    print(i)
+
