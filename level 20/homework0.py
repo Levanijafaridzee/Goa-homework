1e7d74c433ed76f696bdc845dd939b010609d3d5
Levanijafaridzee <levanijafaridzejafaridze@gmail.com>
2024-07-14 03:58:57 +0400

    commit

---
+++ level 20/homework0.py
@@ -0,0 +1,17 @@
+
+#1. davaleba
+def print_words(words):
+    print(words)
+
+print_words("goas is beast")
+
+#2. davaleba
+
+def sum(num,number):
+    print(num+number)
+
+sum(20,30)
+
+
+#3.davaleba
+
---
+++ level 22/homework0.py
@@ -0,0 +1,20 @@
+#1.davaleba
+def print_number(num):
+    print( num * 5 )
+
+print_number(20)
+
+
+#2.
+def print_numbers(num,number):
+    print(num * number)
+
+print_numbers(13,33)
+
+
+#3.davaleba
+
+def print_name(nam):
+    return len(nam + "gio")
+
+print_name("levani")
